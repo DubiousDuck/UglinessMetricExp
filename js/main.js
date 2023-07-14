455850ace52437428c7604d753b70debf98f6b6b
DubiousDuck <oscar517730@gmail.com>
2023-07-13 19:35:31 -0700

    consistency and bugfix

--- js/main.js
+++ js/main.js
@@ -13,8 +13,6 @@ const PRAC_IMG = [
 const TRIAL_NUM = IMG_FILES.length;
 const RATING_PRACTICE_TRIAL_N = 5;
 const RATING_PRACTICE_LIST = shuffle_array(PRAC_IMG);
-let part_num = 1;
-
 const NEG_ADJECTIVES = [
     "gross",
     "ugly",
@@ -23,12 +21,13 @@ const NEG_ADJECTIVES = [
     "sad",
     "scary",
     "bad"
-]
-let randomAdj = shuffle_array(NEG_ADJECTIVES);
-randomAdj[randomAdj.length] = "aesthetically pleasing";
-const RANDOM_ADJ = randomAdj;
+];
+let random_adj = shuffle_array(NEG_ADJECTIVES);
+random_adj[random_adj.length] = "aesthetically pleasing";
+const RANDOM_ADJ = random_adj;
+const INSTR_READING_TIME_MIN = 0.5;
 
-let instr;
+let subj, instr, practice_task, task;
 
 // #### ##    ##  ######  ######## ########
 //  ##  ###   ## ##    ##    ##    ##     ##
@@ -66,7 +65,7 @@ const INSTRUCTIONS = [
         hide_instr_img,
         show_mock_scale,
         "For the first part, I am interested in how <strong>" +
-            RANDOM_ADJ[part_num - 1] +
+            RANDOM_ADJ[0] +
             "</strong> you find each image is. You will rate the image using a seven-point scale, as in the example below."
     ],
     [
@@ -80,21 +79,21 @@ const INSTRUCTIONS = [
         "Now, you are ready for the first part.<br/><br/>Press ENTER to start a few pratice!"
     ],
     [
-        start_practice,
+        false,
         false,
         "Great! You have completed the practice trials. Press ENTER to start for real!"
     ]
 ]; //the attributes are: pre_function, post_function, display text
 
-function show_maximize_window(){
+function show_maximize_window() {
     $("#full-screen-img").css("display", "block");
 }
 
-function hide_maximize_window(){
-    $("#full-screen-img").css("display", "none");
+function hide_maximize_window() {
+    $("#full-screen-img").hide();
 }
 function hide_instr_img() {
-    $("#display-img").css("display", "none");
+    $("#display-img").hide();
 }
 
 function show_instr_img() {
@@ -102,25 +101,23 @@ function show_instr_img() {
     $("#display-img").css("display", "block");
 }
 
-function show_mock_scale(){
+function show_mock_scale() {
     $("#mock-rating").css("display", "flex");
 }
 
-function hide_mock_scale(){
-    $("#mock-rating").css("display", "none");
+function hide_mock_scale() {
+    $("#mock-rating").hide();
 }
 function show_consent() {
-    $("#next-button").css("display", "none");
-    $("#consent-box").css("display", "block");
-    $("#consentResponse").css("display", "block");
+    $("#next-button").hide();
+    $("#consent-box").show();
     $(document).keyup(function (e) {
-        console.log("keypressed!");
         if (e.key == "Enter") {
             $(document).off("keyup");
             instr.next();
-            $("#instr-box").css("display", "none");
-            $("#consent-box").css("display", "none");
-            console.log("it's me, Mario!");
+            $("#instr-box").hide();
+            $("#consent-box").hide();
+            start_task();
         }
     });
 }
@@ -151,19 +148,12 @@ const TASK_TITLES = [
     "rt"
 ];
 
-function start_practice(){
-    $("#prompt-adj").text(RANDOM_ADJ[0]);
-    task = new Task(task_options);
-    $("#task-box").show();
-    task.trialList = shuffle_array(task.trialList);
-    task.run();
-}
 function start_task() {
-    //task_options['subj'] = subj;
-    $("#prompt-adj").text(RANDOM_ADJ[part_num - 1]);
+    $("#prompt-adj").text(RANDOM_ADJ[subj.partNum - 1]);
+    task_options["subj"] = subj;
     task = new Task(task_options);
     $("#task-box").show();
-    //subj.detectVisibilityStart();
+    subj.detectVisibilityStart();
     task.trialList = shuffle_array(task.trialList);
     task.run(); //central function call for task to run
 }
@@ -172,12 +162,11 @@ function start_task() {
 function task_update(formal_trial, last, this_trial, next_trial, path) {
     //use formal_trial parameter to determine whether the trial is practice or formal
     task.stimName = this_trial;
-    $('#part-num').text(part_num);
-    $('#progressBar').text(task.progress);
+    $("#part-num").text(subj.partNum);
+    $("#progressBar").text(task.progress);
     $("#task-img").attr("src", path + this_trial);
-    console.log(this_trial);
     if (formal_trial) {
-        //the first time switched to formal, remind the participants that we are now switiching
+        //the first time switched to formal, remind the participants that we are now switching
         if (task.pracTrialN != 0 && task.trialNum == 1) {
             formal_trial_notice();
         }
@@ -190,16 +179,16 @@ function task_update(formal_trial, last, this_trial, next_trial, path) {
 function formal_trial_notice() {
     instr.startTimer();
     listen_to_start_formal();
-    $("#task-box").css("display", "none");
-    $("#instr-box").css("display", "block");
+    $("#task-box").hide();
+    $("#instr-box").show();
 }
 
 function listen_to_start_formal() {
     $(document).keyup(function (e) {
         if (e.key == "Enter") {
             $(document).off("keyup");
-            $("#instr-box").css("display", "none");
-            $("#task-box").css("display", "block");
+            $("#instr-box").hide();
+            $("#task-box").show();
             task.startTime = Date.now();
         }
     });
@@ -207,8 +196,8 @@ function listen_to_start_formal() {
 
 function rating() {
     $("#task-img").show();
-    $(".ratingButton").mouseup(function (event) {
-        $(".ratingButton").unbind("mouseup");
+    $(".rating-button").mouseup(function (event) {
+        $(".rating-button").unbind("mouseup");
         task.inView = check_fully_in_view($("#task-img"));
         $("#task-img").hide();
         let target = $(event.target).closest(".rating-button"); //choose the closest button to the pointer
@@ -217,41 +206,47 @@ function rating() {
 }
 
 function end_task() {
-    //subj.detectVisibilityEnd();
-    if (part_num < BLOCK_N) inter_block_rest();
-    else end_of_trial();
-    //task.save();
+    task.save();
+    if (subj.partNum < BLOCK_N) inter_block_rest();
+    else end_of_task();
 }
 
 function inter_block_rest() {
-    $("#task-box").css("display", "none");
-    $("#instr-box").css("display", "block");
+    $("#task-box").hide();
     $("#instr-text").html(
         "You have completed " +
-            part_num +
+            subj.partNum +
             " out of " +
             BLOCK_N +
             " parts. Take a brief break if you wish.<br/><br/> Next, I am interested in how <em>" +
-            RANDOM_ADJ[part_num] +
+            RANDOM_ADJ[subj.partNum] +
             "</em> you find each image is.<br/><br/> When you are ready, press ENTER to continue."
     );
+    $("#instr-box").show();
     $(document).keyup(function (e) {
-        console.log("keypressed!");
         if (e.key == "Enter") {
             $(document).off("keyup");
-            $("#instr-box").css("display", "none");
+            $("#instr-box").hide();
             start_next_part();
         }
     });
 }
 function start_next_part() {
     //increase block count and restart task
-    if (part_num == 1) {
+    subj.partNum += 1;
+    if (subj.partNum == 2) {
         task_options.pracTrialN = 0;
     }
-    part_num += 1;
     start_task();
 }
+
+function end_of_task() {
+    //pull up question box
+    subj.detectVisibilityEnd();
+    $("#task-box").hide();
+    $("#question-box").show();
+}
+
 let task_options = {
     titles: TASK_TITLES,
     pracTrialN: RATING_PRACTICE_TRIAL_N,
@@ -269,11 +264,6 @@ let task_options = {
     progressInfo: true
 };
 
-function end_of_trial() {
-    //pull up question box
-    $("#task-box").hide();
-    $("#question-box").show();
-}
 //  ######  ##     ## ########        ## ########  ######  ########
 // ##    ## ##     ## ##     ##       ## ##       ##    ##    ##
 // ##       ##     ## ##     ##       ## ##       ##          ##
@@ -282,53 +272,98 @@ function end_of_trial() {
 // ##    ## ##     ## ##     ## ##    ## ##       ##    ##    ##
 //  ######   #######  ########   ######  ########  ######     ##
 
+const SUBJ_TITLES = [
+    "subjNum",
+    "startDate",
+    "startTime",
+    "id",
+    "endTime",
+    "duration",
+    "instrReadingTimes",
+    "quickReadingPageN",
+    "hiddenCount",
+    "hiddenDurations",
+    "inView",
+    "viewportW",
+    "viewportH",
+    "serious",
+    "maximized",
+    "problems",
+    "age",
+    "gender"
+];
+
 function submit_question() {
-    const openQueNames = ["age", "problems"];
-    const choiceNames = ["serious", "gender"];
-    if (check_answered(openQueNames, choiceNames)) {
-        $("#question-box").css("display", "none");
+    let open_que_names = ["age", "problems"];
+    let choice_names = ["serious", "gender"];
+    if (check_answered(open_que_names, choice_names)) {
+        $("#question-box").hide();
+        for (let q of open_que_names) {
+            subj[q] = $("#" + q)
+                .val()
+                .replace(/(?:\r\n|\r|\n)/g, "<br />");
+        }
+        subj.instrReadingTimes = JSON.stringify(instr.readingTimes);
+        subj.quickReadingPageN = Object.values(instr.readingTimes).filter(
+            (d) => d < INSTR_READING_TIME_MIN
+        ).length;
+        subj.submitAnswers();
         $("#debrief-box").css("display", "flex");
-        $("#endingShortcut").hide();
+        $("#ending-shortcut").hide();
     }
 }
 
-function check_answered(openEndedQue, choiceQue) {
-    let allResponded = true;
-    for (let q of openEndedQue) {
+function check_answered(open_ended_que, choice_que) {
+    let all_responded = true;
+    for (let q of open_ended_que) {
         let value = $("input[name=" + q + "]").val();
         if (value == "") {
             $("#" + q + "-warning").css("display", "block");
-            allResponded = false;
-        } else $("#" + q + "-warning").css("display", "none");
+            all_responded = false;
+        } else $("#" + q + "-warning").hide();
     }
-    for (let q of choiceQue) {
+    for (let q of choice_que) {
         let value = $("input[name=" + q + "]:checked").val();
         if (value === undefined) {
             $("#" + q + "-warning").css("display", "block");
-            allResponded = false;
-        } else $("#" + q + "-warning").css("display", "none");
+            all_responded = false;
+        } else $("#" + q + "-warning").hide();
     }
-    if ($("#problems").val() == ""){
+    if ($("#problems").val() == "") {
         $("#problems-warming").show();
-        allResponded = false;
-    }else $("#problems-warming").hide();
-    return allResponded;
+        all_responded = false;
+    } else $("#problems-warming").hide();
+    return all_responded;
 }
 
-function go_to_top(){
-    $('html, body').animate({ scrollTop: 0 }, 'slow');
+function go_to_top() {
+    $("html, body").animate({ scrollTop: 0 }, "slow");
 }
 
-function go_to_completion_page(){
+function go_to_completion_page() {
     exit_fullscreen();
 }
 
-function go_to_ending(){
+function go_to_ending() {
     $("#instr-box").hide();
     $("#debrief-box").show();
-    $("#endingShortcut").hide();
-    $("#endingShortcut").hide();
+    $("#ending-shortcut").hide();
+    $("#ending-shortcut").hide();
 }
+
+let subj_options = {
+    subjNumScript: "", // XXX
+    savingScript: "", // XXX
+    subjNumFile: "", // XXX
+    visitFile: "visit.txt", // XXX
+    attritionFile: "attrition.txt", // XXX
+    subjFile: "subj.txt", // XXX
+    savingDir: "data/testing", // XXX
+    titles: [""],
+    viewportMinW: 0,
+    viewportMinH: 0
+};
+
 // ########  ########    ###    ########  ##    ##
 // ##     ## ##         ## ##   ##     ##  ##  ##
 // ##     ## ##        ##   ##  ##     ##   ####
@@ -339,6 +374,8 @@ function go_to_ending(){
 
 $(document).ready(function () {
     load_img(0, IMG_FOLDER, IMG_FILES);
+    subj = new Subject(subj_options);
+    subj.partNum = 0;
     instr = new Instructions(instr_options);
     instr.start();
 });
